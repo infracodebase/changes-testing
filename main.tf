17662f404822e99e48d5485a7f13248ca5792557
Justin O'Connor <jsoconno@gmail.com>
2026-01-04 03:02:19 +0000

    feat: add Azure Key Vault with random provider for naming
    Co-Authored-By: Infracodebase <agent@infracodebase.com>

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -21,3 +25,61 @@ resource "azurerm_resource_group" "main" {
 
   tags = var.common_tags
 }
+
+# Get the current Azure AD client configuration
+data "azurerm_client_config" "current" {}
+
+# Create a Key Vault
+resource "azurerm_key_vault" "main" {
+  name                        = "kv-${random_string.suffix.result}"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+      "List",
+      "Create",
+      "Delete",
+      "Update",
+      "Recover",
+      "Purge",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Purge",
+    ]
+
+    storage_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Purge",
+    ]
+  }
+
+  tags = var.common_tags
+}
+
+# Generate a random suffix for unique naming
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
